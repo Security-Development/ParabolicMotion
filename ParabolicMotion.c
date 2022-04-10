9a3b013b813ac459b846ea1b3bfe8d035e84cc7b
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-04-10 15:58:51 +0900

    Update ParabolicMotion.c

--- ParabolicMotion.c
+++ ParabolicMotion.c
@@ -3,9 +3,9 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-void mPrint(char m[32][32]){
-    for(int i = 31; i > 0; i--){
-        for(int j = 0; j < 32; j++){
+void mPrint(char m[64][64]){
+    for(int i = 63; i > 0; i--){
+        for(int j = 0; j < 64; j++){
             printf("%c ", m[i][j]);
         }
         printf("\n");
@@ -16,17 +16,17 @@ int main() {
     double vx, vy;
     double v, angle, gravity, radian, time, result, h;
 
-    char map[32][32]; // map 값을 0으로 초기화
+    char map[64][64]; // map 값을 0으로 초기화
 
-    for(int i = 0; i < 32; i++){
-        for(int j = 0; j < 32; j++){
-            map[i][j] = '□';
+    for(int i = 0; i < 64; i++){
+        for(int j = 0; j < 64; j++){
+            map[i][j] = '  ';
         }
     }
 
     v = 5; // 초기 속도
-    angle = 70; // 던지는 각도 설정
-    gravity = 0.5; // 중력
+    angle = 45; // 던지는 각도 설정
+    gravity = 1; // 중력
     radian = angle * M_PI / 180; // 각도를 호도법으로 변환
     time = 0; // 시간
 
@@ -39,10 +39,10 @@ int main() {
         vx = v * cos(radian) * time; // x의 시간 변화량 좌표
         vy = (v * sin(radian)* time) - (0.5 * gravity * pow(time, 2)); // y의 시간 변화량 좌표
 
-        map[(int)vy][(int)vx] = 'A';
+        map[(int)vy][(int)vx] = 'O';
         printf("vx : %f \nvy : %f\nh => %f\n\n", vx, vy, h);
 
-        time += 1;
+        time += 0.1;
 
     }
 
